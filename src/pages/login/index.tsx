08a579c1c0cfe3ed595a245cf3ddd1402b9aebbd
Befous <befousmain@gmail.com>
2023-12-17 16:35:07 +0700

    test get env

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,11 @@
 import FormLogin from "@/components/formlogin"
 
 export default function Page() {
+  console.log(process.env.DB_HOST)
+  var test = String(process.env.DB_HOST);
   return (
     <>
-      <h1>Ini login</h1>
+      <h1>{test}</h1>
       <FormLogin />
     </>
   )
