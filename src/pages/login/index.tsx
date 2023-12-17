83b9af7c2ceac706e83a15d01255b77a0ad87915
Befous <befousmain@gmail.com>
2023-12-17 16:55:48 +0700

    Test .env

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,9 +4,11 @@ export default function Page() {
   console.log(process.env.DB_HOST);
   console.log(process.env.NEXT_PUBLIC_DB_USER);
   console.log(process.env.DB_PASS);
+  var testpublic = (process.env.NEXT_PUBLIC_DB_USER);
   return (
     <>
       <h1>Test Login</h1>
+      <h1>{testpublic}</h1>
       <FormLogin />
     </>
   )
